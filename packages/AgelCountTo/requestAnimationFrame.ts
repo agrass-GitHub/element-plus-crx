8ba92c8bd3551cc5130535106e964125608840ee
agrass <992951674@qq.com>
2023-03-14 18:07:50 +0800

    fix

--- packages/AgelCountTo/requestAnimationFrame.ts
+++ packages/AgelCountTo/requestAnimationFrame.ts
@@ -1,23 +1,40 @@
 let lastTime = 0;
 
-let requestAnimationFrame = window.requestAnimationFrame;
-let cancelAnimationFrame = window.cancelAnimationFrame;
+const isServer = typeof window === 'undefined'
 
-if (!requestAnimationFrame || !cancelAnimationFrame) {
-  requestAnimationFrame = function (callback: (time: number) => void) {
-    const currTime = new Date().getTime();
-    // 为了使setTimteout的尽可能的接近每秒60帧的效果
-    const timeToCall = Math.max(0, 16 - (currTime - lastTime));
-    const id = window.setTimeout(() => {
-      callback(currTime + timeToCall);
-    }, timeToCall);
-    lastTime = currTime + timeToCall;
-    return id;
-  };
+let requestAnimationFrame
+let cancelAnimationFrame
 
-  cancelAnimationFrame = function (id: number) {
-    window.clearTimeout(id);
-  };
+
+if(isServer){
+  requestAnimationFrame = function () {
+    return
+  }
+  cancelAnimationFrame = function () {
+    return
+  }
+}else{
+  requestAnimationFrame = window.requestAnimationFrame
+  cancelAnimationFrame = window.cancelAnimationFrame
+
+  if (!requestAnimationFrame || !cancelAnimationFrame) {
+    requestAnimationFrame = function (callback: (time: number) => void) {
+      const currTime = new Date().getTime();
+      // 为了使setTimteout的尽可能的接近每秒60帧的效果
+      const timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      const id = window.setTimeout(() => {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  
+    cancelAnimationFrame = function (id: number) {
+      window.clearTimeout(id);
+    };
+  }
 }
+
+
 
 export { requestAnimationFrame, cancelAnimationFrame };
