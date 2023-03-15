8fe4e45c93f6383a0d623320053e882e6f4d63c4
agrass <992951674@qq.com>
2023-03-15 17:16:38 +0800

    add

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,7 @@
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AgelFormItem: typeof import('./packages')['AgelFormItem'],
+    AgelFormItems: typeof import('./packages')['AgelFormItems'],
     AgelFormGrid: typeof import('./packages')['AgelFormGrid'],
     AgelFormDesc: typeof import('./packages')['AgelFormDesc'],
     AgelFormTableditor: typeof import('./packages')['AgelFormTableditor'],
